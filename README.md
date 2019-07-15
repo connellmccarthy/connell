af530bdc6335c4442daf0cc73d22f56d7d752ee0
Connell McCarthy <connellmccarthy@gmail.com>
2019-07-14 20:00:24 -0400

    Initial Setup
    All files for V4

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# connell
+connellmccarthy.com
+======
+My personal portfolio landing page.
+
+### Uses the following libraries
+
+[Skeleton.css](http://getskeleton.com/)
+
+[pace.js](https://github.hubspot.com/pace/docs/welcome/)
---
+++ bin/js/main.js
@@ -0,0 +1,23 @@
+var isMobile = window.matchMedia("only screen and (max-width: 768px)").matches;
+var count = 1;
+var max = 4;
+var categ = ["Branding", "Branding", "Digital Art", "Digital Art"];
+$(document).ready(function() {
+	console.log('%cHandcrafted from scratch just for you. \nv4 - Connell McCarthy \n\nView on Github: https://github.com/connellmccarthy/connell',"border-radius:3px; padding:3px; background: #FF6136; color: #fff");
+	if (isMobile) {
+		window.setInterval(function(){switchFrame('fwd');}, 2500);
+	}
+	$('.next').click(function() {switchFrame('fwd');});
+	$('.prev').click(function() {switchFrame('rvs');});
+});
+
+function switchFrame(d) {
+	if (d == 'fwd'){
+		if (count == max) {count = 1;} else {count ++;}
+	} else {
+		if (count == 1) {count = max;} else {count --;}
+	}
+	$('#w-in-n').text(count);
+	$('#w-in-t').text(categ[count - 1]);
+	$('#work-img').attr('src', '/img/work/' + count + '.jpg');
+}
