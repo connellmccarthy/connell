4a607cc1273aeff4e0e5974d0b16851be1fbc8f3
Connell McCarthy <connell.mccarthy@shopify.com>
2019-07-16 18:44:16 -0400

    Updated enhancementsd
    Meta info, navigation fixes, animations

--- bin/js/main.js
+++ bin/js/main.js
@@ -4,12 +4,24 @@ var max = 4;
 var categ = ["Branding", "Branding", "Digital Art", "Digital Art"];
 $(document).ready(function() {
 	console.log('%cHandcrafted from scratch just for you. \nv4 - Connell McCarthy \n\nView on Github: https://github.com/connellmccarthy/connell',"border-radius:3px; padding:3px; background: #FF6136; color: #fff");
+	preload([
+	    'img/work/1.jpg',
+	    'img/work/2.jpg',
+	    'img/work/3.jpg',
+	    'img/work/4.jpg'
+	]);
 	if (isMobile) {
 		window.setInterval(function(){switchFrame('fwd');}, 2500);
 	}
 	$('.next').click(function() {switchFrame('fwd');});
 	$('.prev').click(function() {switchFrame('rvs');});
 });
+
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
 
 function switchFrame(d) {
 	if (d == 'fwd'){
